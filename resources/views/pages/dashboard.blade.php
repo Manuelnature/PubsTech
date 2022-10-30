9cee0770f6507dd3c101fb30fce48ab4690411ac
Manuelnature <manuelnature58@gmail.com>
2022-10-30 20:01:24 +0000

    changes made

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -194,6 +194,86 @@
             <div class="col-md-12">
               <div class="card">
                 <div class="card-header">
+                  <h5 class="card-title">Overall Sales
+                    <span style="margin-left:40px !important">
+                        {{ \Carbon\Carbon::parse($date_from)->format('j F, Y')}}
+                        <strong> &nbsp;-&nbsp; </strong>
+                        {{ \Carbon\Carbon::parse($date_to)->format('j F, Y')}}
+                    </span>
+                  </h5>
+
+                  <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                      <i class="fas fa-minus"></i>
+                    </button>
+                    <button type="button" class="btn btn-tool" data-card-widget="remove">
+                      <i class="fas fa-times"></i>
+                    </button>
+                  </div>
+                </div>
+                <!-- /.card-header -->
+
+
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-12 col-sm-6 col-md-6">
+                          <div class="info-box mb-3">
+                            <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-shopping-cart"></i></span>
+
+                            <div class="info-box-content">
+                              <span class="info-box-text">Total Stocks Sold</span>
+                              <span class="info-box-number">{{ $overall_total_quantity_sold }}</span>
+                            </div>
+                            <!-- /.info-box-content -->
+                          </div>
+                          <!-- /.info-box -->
+                        </div>
+                        <!-- /.col -->
+
+                        <!-- fix for small devices only -->
+                        <div class="clearfix hidden-md-up"></div>
+
+                        <div class="col-12 col-sm-6 col-md-6">
+                          <div class="info-box mb-3">
+                            <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-coins"></i></span>
+
+                            <div class="info-box-content">
+                              <span class="info-box-text">Expected Amount</span>
+                              <span class="info-box-number">
+                                @php
+                                    echo 'Gh¢ '.number_format($overall_total_expected_price, 2);
+                                @endphp
+                              </span>
+                            </div>
+                            <!-- /.info-box-content -->
+                          </div>
+                          <!-- /.info-box -->
+                        </div>
+                        <!-- /.col -->
+                      </div>
+                  <!-- /.row -->
+                </div>
+                <!-- ./card-body -->
+                <div class="card-footer">
+
+
+                </div>
+                <!-- /.card-footer -->
+              </div>
+              <!-- /.card -->
+            </div>
+            <!-- /.col -->
+          </div>
+          <!-- /.row -->
+        </div><!--/. container-fluid -->
+    </section>
+
+    <section class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="card">
+                <div class="card-header">
                   <h5 class="card-title" style="font-weight: 700; color:#0096FF">Summary Of My Sales Records For Today </h5>
 
                   <div class="card-tools">
